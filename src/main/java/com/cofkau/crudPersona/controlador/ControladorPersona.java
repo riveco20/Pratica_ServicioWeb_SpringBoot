ddade4789a94df3dd918410d2109a1723c7b5a95
Richard Vellojin Coneo <richardvellojin@gmail.com>
2021-09-19 17:25:12 -0500

    Confirmando conexion a base de dato

--- src/main/java/com/cofkau/crudPersona/controlador/ControladorPersona.java
+++ src/main/java/com/cofkau/crudPersona/controlador/ControladorPersona.java
@@ -1,12 +1,16 @@
 package com.cofkau.crudPersona.controlador;
 
 import com.cofkau.crudPersona.entidades.Persona;
+import com.cofkau.crudPersona.excepciones.EscrituraDatosEx;
+import com.cofkau.crudPersona.excepciones.LecturaDatosEx;
 import com.cofkau.crudPersona.servicios.InterfasServiciosPersona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
-@RequestMapping("api")
+@RequestMapping("api/Persona")
 
 public class ControladorPersona {
 
@@ -16,33 +20,36 @@ public class ControladorPersona {
     @GetMapping(value = "/listarPersonas")
     public Iterable<Persona>listarPersonas(){
 
-        return servicio.listar();
+        try {
+            return servicio.listar();
+        } catch (LecturaDatosEx e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @PostMapping(value = "/guardarPersona")
-    public Persona guaradarPersona(@RequestBody Persona persona){
+    public Persona guaradarPersona(@RequestBody Persona persona) throws EscrituraDatosEx {
         if(persona!=null){
             System.out.println("El objeto no tiene valor");
         }
-
-
         return servicio.guardar(persona);
     }
 
     @GetMapping(value = "/buscarPersona")
-    public Persona buscarPersona(@PathVariable int id){
+    public Optional<Persona> buscarPersona(@PathVariable int id) throws LecturaDatosEx {
                 return servicio.listarId(id);
     }
 
 
     @DeleteMapping(value ="/eliminarPersona")
-    public Persona eliminarPersona(@PathVariable int id){
+    public Optional<Persona> eliminarPersona(@PathVariable int id) throws LecturaDatosEx {
         return servicio.borrar(id);
 
     }
 
     @PutMapping(value = "/actualizarPersona")
-    public Persona actualizarPersona(@RequestBody Persona persona){
+    public Persona actualizarPersona(@RequestBody Persona persona) throws LecturaDatosEx {
         return servicio.actualizar(persona);
     }
 
